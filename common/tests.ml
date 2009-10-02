b86a48717399a50cf887c05d4044db538c424708
Pietro Abate <Pietro.Abate@pps.jussieu.fr>
2009-10-02 13:00:54 +0000

    - small refactoring of the common oUnit tests

--- common/tests.ml
+++ common/tests.ml
@@ -12,47 +12,69 @@
 open OUnit
 open Common
 
+let test_deb =
+  "deb" >:: (fun _ ->
+    let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
+      Uri.parseUri "deb://path/to/file" 
+    in assert_equal true (protocol = "deb" && path = "path/to/file")
+  )
+;;
+
+let test_hdlist = 
+  "hdlist" >:: (fun _ ->
+    let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
+      Uri.parseUri "hdlist://path/to/file" 
+    in assert_equal true (protocol = "hdlist" && path = "path/to/file")
+  )
+;;
+
+let test_synth =
+  "synth" >:: (fun _ ->
+    let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
+      Uri.parseUri "synth://path/to/file" 
+    in assert_equal true (protocol = "synth" && path = "path/to/file")
+  )
+;;
+
+let test_cudf = 
+  "cudf" >:: (fun _ ->
+    let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
+      Uri.parseUri "cudf:///path/to/file" 
+    in assert_equal true (protocol = "cudf" && path = "/path/to/file")
+  )
+;;
+
+let test_sqlite =
+  "sqlite" >:: (fun _ ->
+    let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
+      Uri.parseUri "sqlite:///path/to/file" 
+    in assert_equal true (protocol = "sqlite" && path = "/path/to/file")
+  ) 
+;;
+
+(* TODO : I know that the dbname and queryOpt are wrong *)
+let test_pgsql =
+  "pgsql" >:: (fun _ ->
+    let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
+      Uri.parseUri "pgsql://test:tester@localhost/dbname?v1=k1&v2=k2"
+    in assert_equal true (
+      protocol = "pgsql" && 
+      userOpt = Some "test" &&
+      passOpt = Some "tester" &&
+      hostOpt = Some "localhost" &&
+      path = "dbname" &&
+      queryOpt = Some "v1=k1&v2=k2"
+    )
+  ) 
+;;
+
 let parse_uri =
   "parse uri" >::: [
-    "deb" >:: (fun _ ->
-      let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
-        Uri.parseUri "deb://path/to/file" 
-      in assert_equal true (protocol = "deb" && path = "path/to/file")
-    ) ;
-    "hdlist" >:: (fun _ ->
-      let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
-        Uri.parseUri "hdlist://path/to/file" 
-      in assert_equal true (protocol = "hdlist" && path = "path/to/file")
-    ) ;
-    "synth" >:: (fun _ ->
-      let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
-        Uri.parseUri "synth://path/to/file" 
-      in assert_equal true (protocol = "synth" && path = "path/to/file")
-    ) ;
-
-    "cudf" >:: (fun _ ->
-      let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
-        Uri.parseUri "cudf:///path/to/file" 
-      in assert_equal true (protocol = "cudf" && path = "/path/to/file")
-    ) ;
-    "sqlite" >:: (fun _ ->
-      let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
-        Uri.parseUri "sqlite:///path/to/file" 
-      in assert_equal true (protocol = "sqlite" && path = "/path/to/file")
-    ) ;
-    (* TODO : I know that the dbname and queryOpt are wrong *)
-    "pgsql" >:: (fun _ ->
-      let (protocol,(userOpt,passOpt,hostOpt,portOpt,path),queryOpt) =
-        Uri.parseUri "pgsql://test:tester@localhost/dbname?v1=k1&v2=k2"
-      in assert_equal true (
-        protocol = "pgsql" && 
-        userOpt = Some "test" &&
-        passOpt = Some "tester" &&
-        hostOpt = Some "localhost" &&
-        path = "dbname" &&
-        queryOpt = Some "v1=k1&v2=k2"
-      )
-    ) ;
+    test_deb;
+    test_hdlist;
+    test_synth;
+    test_cudf;
+    test_sqlite;
   ]
 
 (*
