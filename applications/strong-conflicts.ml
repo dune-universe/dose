be4b2b301cd38e513e70d9770b109e695c17296e
Jaap Boender <boender@pps.jussieu.fr>
2009-10-07 12:25:42 +0000

    - Strong conflicts application seems to work now

--- applications/strong-conflicts.ml
+++ applications/strong-conflicts.ml
@@ -36,66 +36,75 @@ begin
         | V.Or _ -> G.fold_pred (fun v' l' ->
           match v' with
           | V.Pkg p'' -> p''::l'
-          | V.Or _ -> failwith "inconsistent dependency graph"
+          | V.Or _ -> failwith "Inconsistent dependency graph (Or -> Or)"
         ) gr v l
       end
     | _ -> l
    ) gr p []
 end;;
 
-let add_pair (l, r) =
-begin
-  Printf.eprintf "pair: %s, %s" (string_of_pkgname l.package) (string_of_pkgname r.package)
-end
+type conflict_type = Conjunctive | Other;;
+let nr_conj_pairs = ref 0;;
+let nr_other_pairs = ref 0;;
 
-let rec explode_direct_dependencies gr c1_before c2_before c1 c2 =
-(* c1/c2_before: list of packages already considered on left/right-hand side
- * c1/c2: list of packages to be considered on the left/righ5-hand side
- * Algorithm:
- * - find all conjunctive predecessors of packages in c1 [-> c1_pred]
- * - find all conjunctive predecessors of packages in c2 [-> c2_pred]
- * - add strong conflicts between every element of
- *   c1_pred and [c2 @ c2_before] 
- * - add strong conflicts between every element of
- *   c2_pred and [c1 @ c1_before]
- * - add strong conflicts between every element of c1_pred and c2_pred 
- * - recurse. *)
+let add_pair pair_ht x y ct =
+let (c1, c2) = if x < y then (x, y) else (y, x) in
 begin
-  let c1work = c1 @ c1_before
-  and c2work = c2 @ c2_before in
-  let c1_pred =
-    List.fold_left (fun l c1p ->
-      if List.mem c1p c1work then
-        l
-      else
-        List.fold_left (fun l' x ->
-          match x with
-          | V.Pkg x' -> x'::l'
-          | _ -> l'
-        ) l (G.pred gr (V.Pkg c1p))
-    ) [] c1
-  and c2_pred = 
-    List.fold_left (fun l c1p ->
-      if List.mem c1p c1work then
-        l
-      else
-        List.fold_left (fun l' x ->
-          match x with
-          | V.Pkg x' -> x'::l'
-          | _ -> l'
-        ) l (G.pred gr (V.Pkg c1p))
-    ) [] c2 in
-    List.iter (fun l ->
-      List.iter (fun r ->
-        add_pair (l, r)
-      ) (c2_pred @ c2work)
-    ) c1_pred;
-    List.iter (fun l ->
-      List.iter (fun r ->
-        add_pair (l, r)
-      ) c1work
-    ) c2_pred;
-    explode_direct_dependencies gr c1work c2work c1_pred c2_pred
+  try
+    let c1_ht = Hashtbl.find pair_ht c1 in
+    if not (Hashtbl.mem c1_ht c2) then
+    begin
+      Hashtbl.add c1_ht c2 ct;
+      match ct with
+      | Conjunctive -> incr nr_conj_pairs
+      | Other -> incr nr_other_pairs
+    end
+  with Not_found ->
+  begin
+    let c1_ht = Hashtbl.create 128 in
+    Hashtbl.add c1_ht c2 ct;
+    Hashtbl.add pair_ht c1 c1_ht;
+    match ct with
+    | Conjunctive -> incr nr_conj_pairs
+    | Other -> incr nr_other_pairs
+  end
+end;;
+
+let add_predecessors gr c_pred_ht pred_ht c =
+  let visited = Hashtbl.create 64 in
+  let rec visit_conj acc = function
+    [] -> acc
+  | p::r ->
+    if Hashtbl.mem visited p then visit_conj acc r
+    else
+    begin
+      Hashtbl.add visited p true;
+      visit_conj (p::acc)
+        (G.fold_pred_e (fun (_,ct,p) l ->
+          match ct with
+          | E.DirDepends ->
+            begin
+              match p with
+              | V.Pkg p' -> p'::l
+              | _ -> failwith "Inconsistent dependency graph (Or -> DirDepends)"
+            end
+          | _ -> l
+        ) gr (V.Pkg p) r)
+    end in
+  let rec visit_disj acc = function
+    [] -> acc
+  | p::r ->
+    if Hashtbl.mem visited p then visit_disj acc r
+    else
+    begin
+      Hashtbl.add visited p true;
+      visit_disj (p::acc) (preceding_packages gr (V.Pkg p)@r)
+    end in
+begin
+  let c_pred = visit_conj [] [c] in
+    Hashtbl.add c_pred_ht c c_pred;
+    Hashtbl.add pred_ht c (visit_disj c_pred 
+      (List.fold_left (fun l p -> l@preceding_packages gr (V.Pkg p)) [] c_pred))
 end;;
 
 let _ =
@@ -115,7 +124,7 @@ begin
   Printf.eprintf "Parsing...%!";
   (* let timer = Util.Timer.create "Parsing" in
   Util.Timer.start timer; *)
-  let pl = match Input.parse_uri !uri with
+  let u = match Input.parse_uri !uri with
   | ("deb", (_,_,_,_,file),_) ->
     begin
       let l = Debian.Packages.input_raw [file] in
@@ -124,30 +133,32 @@ begin
   | (s, _, _) -> failwith (Printf.sprintf "%s: not supported\n" s) in
   (* ignore (Util.Timer.stop timer ()); *)
   Printf.eprintf "done\n%!";
-  
+
+  let slv = Depsolver.load u in
+  let u = Cudf.load_universe (Depsolver.trim (Depsolver.load u) (Cudf.get_packages u)) in
+  Printf.eprintf "Trimmed: %d\n" (universe_size u);
+
   Printf.eprintf "Generating dependency graph...%!";
-  let gr = Graph.dependency_graph (pl) in
+  let gr = Graph.dependency_graph u in
   Printf.eprintf "done\n%!";
 
   Printf.eprintf "Enumerating conflicts...%!";
   let timer = Util.Timer.create "Enumerating conflicts" in
   Util.Timer.start timer;
-  let m = CudfAdd.build_maps pl in
+  let m = CudfAdd.build_maps u in
   let cl = Cudf.fold_packages (fun l p ->
     List.fold_left (fun l' c ->
       if c < p then
       begin
-       log (Printf.sprintf "%s (%s) <-> %s (%s)\n" (string_of_pkgname p.package)
-          (String.concat "," (List.map (fun x -> (string_of_pkgname x.package)) (preceding_packages gr (V.Pkg p))))
+       log (Printf.sprintf "%s <-> %s \n" (string_of_pkgname p.package)
           (string_of_pkgname c.package)
-          (String.concat "," (List.map (fun x -> (string_of_pkgname x.package)) (preceding_packages gr (V.Pkg c))))
         );
         (c,p)::l'
       end
       else
         l'
     ) l (m.CudfAdd.who_conflicts p)
-  ) [] pl in
+  ) [] u in
   Printf.eprintf "done\n";
   ignore (Util.Timer.stop timer ());
   Printf.eprintf "Explicit conflicts found: %d\n%!" (List.length cl);
@@ -188,9 +199,46 @@ begin
   Printf.eprintf "Exploding direct dependencies...%!";
   let timer = Util.Timer.create "Exploding direct dependencies" in
   Util.Timer.start timer;
+  let pred_ht = Hashtbl.create (2 * List.length clf) in
+  let c_pred_ht = Hashtbl.create (2 * List.length clf) in
+  let pair_ht = Hashtbl.create 8192 in
   List.iter (fun (c1, c2) ->
-    explode_direct_dependencies gr [] [] [c1] [c2]
+    add_predecessors gr c_pred_ht pred_ht c1;
+    add_predecessors gr c_pred_ht pred_ht c2;
+    List.iter (fun c1p ->
+      List.iter (fun c2p ->
+        if c1p <> c2p then add_pair pair_ht c1p c2p Conjunctive
+      ) (Hashtbl.find c_pred_ht c2)
+    ) (Hashtbl.find c_pred_ht c1);
+  ) clf;
+  List.iter (fun (c1, c2) ->
+    List.iter (fun c1p ->
+      List.iter (fun c2p ->
+        if c1p <> c2p then add_pair pair_ht c1p c2p Other
+      ) (Hashtbl.find pred_ht c2)
+    ) (Hashtbl.find pred_ht c1)
   ) clf;
   Printf.eprintf "done\n";
   ignore (Util.Timer.stop timer ());
+
+  let p = Util.Progress.create "Checking non-conjunctive pairs" in
+  Util.Progress.set_total p !nr_other_pairs;
+  let timer = Util.Timer.create "Checking non-conjunctive pairs" in
+  Util.Timer.start timer;
+  Hashtbl.iter (fun c1 c1_ht ->
+    Hashtbl.iter (fun c2 ct ->
+      match ct with
+      | Conjunctive -> Printf.printf "conjunctive: %s <-> %s\n%!" (string_of_pkgname c1.package) (string_of_pkgname c2.package) 
+      | Other -> 
+        let d = Depsolver.edos_coinstall slv [c1;c2] in
+        begin
+          Util.Progress.progress p;
+          match d.Diagnostic.result with
+          | Diagnostic.Failure _ -> Printf.printf "other: %s <-> %s\n%!" (string_of_pkgname c1.package) (string_of_pkgname c2.package)
+          | _ -> () 
+        end
+    ) c1_ht
+  ) pair_ht;
+  ignore (Util.Timer.stop timer ());
+
 end;;
