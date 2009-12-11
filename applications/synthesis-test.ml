dfe6b7730f7cf6fee8cc014a2343957c01f5047a
Jaap Boender <boender@pps.jussieu.fr>
2009-12-11 16:37:01 +0000

    - code cleanup

--- applications/synthesis-test.ml
+++ applications/synthesis-test.ml
@@ -36,23 +36,39 @@ begin
   let s_map = CudfAdd.build_maps su in
   Printf.eprintf "done.\n%!";
 
+  (* check all packages in the synthesis hdlist *)
   Cudf.iter_packages (fun sp ->
     (* find any file provides in sp *)
     List.iter (fun (n, v) ->
       if n.[0] = '/' then
       begin
-        let s_provides = s_map.CudfAdd.who_provides ((n, v) :> Cudf_types.vpkg) in
-        let h_provides = h_map.CudfAdd.who_provides ((n, v) :> Cudf_types.vpkg) in
-        let slist = List.map (fun pn -> string_of_pkgname pn.package) (List.unique (List.sort s_provides)) in
-        let hlist = List.map (fun pn -> string_of_pkgname pn.package) (List.unique (List.sort h_provides)) in
+        let s_provides =
+          s_map.CudfAdd.who_provides ((n, v) :> Cudf_types.vpkg) in
+        let h_provides =
+          h_map.CudfAdd.who_provides ((n, v) :> Cudf_types.vpkg) in
+        let slist =
+          List.map (fun pn ->
+            string_of_pkgname pn.package)
+          (List.unique (List.sort s_provides)) in
+        let hlist =
+          List.map (fun pn ->
+            string_of_pkgname pn.package)
+          (List.unique (List.sort h_provides)) in
+        (* if the lengths are different, there is a problem *)
         if List.length slist <> List.length hlist
         then
-          Printf.printf "ERROR: for package %s-%s, file dependency on %s:\nsynthesis says: %s\nhdlist says:%s\n"
-          (string_of_pkgname sp.package) (string_of_version sp.version) (string_of_pkgname n) (String.concat ", " slist) (String.concat ", " hlist)
-        else if not (List.fold_left2 (fun acc s h -> acc && s = h) true slist hlist)
+          Printf.printf "ERROR: for package %s-%s, file dependency/conflict on %s:\nsynthesis says: %s\nhdlist says:%s\n"
+          (string_of_pkgname sp.package) (string_of_version sp.version)
+          (string_of_pkgname n) (String.concat ", " slist)
+          (String.concat ", " hlist)
+        (* otherwise, check if the lists have the same elements *)
+        else if not
+          (List.fold_left2 (fun acc s h -> acc && s = h) true slist hlist)
         then
-          Printf.printf "ERROR: for package %s-%s, file dependency on %s:\nsynthesis says: %s\nhdlist says:%s\n"
-          (string_of_pkgname sp.package) (string_of_version sp.version) (string_of_pkgname n) (String.concat ", " slist) (String.concat ", " hlist)
+          Printf.printf "ERROR: for package %s-%s, file dependency/conflict on %s:\nsynthesis says: %s\nhdlist says:%s\n"
+          (string_of_pkgname sp.package) (string_of_version sp.version)
+          (string_of_pkgname n) (String.concat ", " slist)
+          (String.concat ", " hlist)
       end
     ) ((List.flatten sp.depends) @ sp.conflicts)
   ) su
