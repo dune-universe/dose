b5700d00abc915147c64ea45c309dfb8266d68fd
Pietro Abate <Pietro.Abate@pps.jussieu.fr>
2010-02-03 14:15:04 +0000

    - add html output - cleanup options parsing - add ordering option - add headers (we can now associate the name of a solver to each solution)

--- applications/cudf-diff.ml
+++ applications/cudf-diff.ml
@@ -15,22 +15,6 @@ open ExtLib
 open Common
 open CudfAdd
 
-module Options = struct
-  open OptParse
-
-  let debug = StdOpt.store_true ()
-  let solution = StdOpt.store_true ()
-  let compare = StdOpt.store_true ()
-
-  let description = "By default we show only the number of broken packages"
-  let options = OptParser.make ~description:description ()
-
-  open OptParser
-  add options ~short_name:'d' ~long_name:"debug" ~help:"Print debug information" debug;
-  add options ~short_name:'s' ~long_name:"solution" ~help:"consider file2 as a solution to the cudf problem in file1 and print the complete list of installed packages" solution;
-  add options ~short_name:'c' ~long_name:"compare" ~help:"compare solutions file2 file3 for the cudf problem in file1" compare;
-end
-
 type status = 
   |Upgraded of Cudf.package (* there exists a newer version in the solution *)
   |Downgraded of Cudf.package (* there exists an older version in the solution *)
@@ -38,18 +22,72 @@ type status =
   |Removed (* this package is not installed anymore *)
   |Unchanged (* this package was installed before and it is now *)
 
+type cmp_t = Up | Down | Inst | Rem | Un
+
 type solution = {
   mutable removed : int ;
   mutable downgraded : int;
-  mutable updated : int;
+  mutable upgraded : int;
   mutable newinst : int;
   mutable unchanged : int;
 }
 
+module Options = struct
+  open OptParse
+
+  exception Format
+  let out_option ?default ?(metavar = "<txt|html>") () =
+    let corce = function
+      |"txt" -> "txt"
+      |"html" -> "html"
+      | _ -> raise Format
+    in
+    let error _ s = Printf.sprintf "%s format not supported" s in
+    Opt.value_option metavar default corce error
+
+  let order_option ?default ?(metavar = "<Un:Up:Down:Inst:Rem>") () =
+    let corce s =
+      let sl = Str.split (Str.regexp ":") s in
+      if sl = [] then raise Format
+      else
+        List.map (function
+          |"Up" -> Up
+          |"Un" -> Un
+          |"Down" -> Down
+          |"Inst" -> Inst
+          |"Rem" -> Rem
+          | _ -> raise Format
+        ) sl
+    in
+    let error _ s = Printf.sprintf "%s ordering not supported" s in
+    Opt.value_option metavar default corce error
+
+  let debug = StdOpt.store_true ()
+  let output = out_option ~default:"txt" ()
+  let order = order_option ~default:[Un;Up;Down;Inst;Rem] ()
+
+  let description = "Compare two or more solutions. Format : solvername:solutionfile"
+  let options = OptParser.make ~description:description ()
+
+  open OptParser
+  add options ~short_name:'d' ~long_name:"debug" ~help:"Print debug information" debug;
+  add options ~short_name:'t' ~long_name:"output" ~help:"Output type" output;
+  add options ~short_name:'o' ~long_name:"order" ~help:"Order" order;
+end
+
+let compare_lex l s1 s2 =
+  List.fold_left (fun acc -> function
+    |Un -> s1.unchanged - s2.unchanged
+    |Up -> s1.upgraded - s2.upgraded
+    |Inst -> s1.newinst - s2.newinst
+    |Down -> s1.downgraded - s2.downgraded
+    |Rem -> s1.removed - s2.removed
+  ) 0 l
+
 let dummy_solution () = {
   removed = 0 ;
   downgraded = 0;
-  updated = 0;
+  upgraded = 0;
   newinst = 0;
   unchanged = 0;
 }
@@ -63,8 +101,14 @@ let status_to_string pkg = function
 
 let solution_to_string s =
   Printf.sprintf
-  "removed = %d\nupdated = %d\ndowngraded = %d\nnew = %d\nunchanged = %d\n"
-  s.removed s.updated s.downgraded s.newinst s.unchanged
+  "removed = %d\nupgraded = %d\ndowngraded = %d\nnew = %d\nunchanged = %d\n"
+  s.removed s.upgraded s.downgraded s.newinst s.unchanged
+;;
+
+let solution_to_html s =
+  Printf.sprintf
+  "<td>removed = %d<br>upgraded = %d<br>downgraded = %d<br>new = %d<br>unchanged = %d</td>"
+  s.removed s.upgraded s.downgraded s.newinst s.unchanged
 ;;
 
 let status_equal = function
@@ -103,7 +147,7 @@ let diff univ sol =
             if Cudf.version_matches p.version (Some(`Eq,pkg.version))
             then begin s.unchanged<-s.unchanged+1 ; Cudf_hashtbl.add h pkg Unchanged end
             else if Cudf.version_matches p.version (Some(`Gt,pkg.version))
-            then begin s.updated<-s.updated+1 ; Cudf_hashtbl.add h pkg (Upgraded p) end
+            then begin s.upgraded<-s.upgraded+1 ; Cudf_hashtbl.add h pkg (Upgraded p) end
             else if Cudf.version_matches p.version (Some(`Lt,pkg.version))
             then begin s.downgraded<-s.downgraded+1 ; Cudf_hashtbl.add h pkg (Downgraded p) end
           ) l
@@ -117,14 +161,10 @@ let diff univ sol =
   (h,s)
 ;;
 
-let print_diff universe solutions =
-  let first = ref true in
-  let cmp (_,(_,s1)) (_,(_,s2)) =
-    if s1.unchanged = s2.unchanged then
-      s1.updated - s2.updated
-    else s1.unchanged - s2.unchanged 
-  in
+let print_diff_txt universe hl solutions =
+  let cmp (_,(_,s1)) (_,(_,s2)) = compare_lex (OptParse.Opt.get Options.order) s1 s2 in
   let solutions = List.sort ~cmp:cmp solutions in
+  Printf.printf "Package | %s\n" (String.concat " | " hl) ;
   Cudf.iter_packages (fun pkg ->
     let pl = 
       List.filter_map (fun (f,(h,_)) ->
@@ -135,16 +175,36 @@ let print_diff universe solutions =
     if all_equal (List.map snd pl) then ()
     else begin
       let sl = List.map (fun (f,status) -> status_to_string pkg status) pl in
-      if !first then begin
-        let hl = List.map (fun (f,s) -> f) pl in
-        Printf.printf "Package | %s\n" (String.concat " | " hl) ;
-        first := false
-      end
-      ;
       Printf.printf "%s %d | %s\n" pkg.package pkg.version (String.concat " | " sl)
     end
   ) universe
 ;;
+
+let print_diff_html universe hl solutions =
+  let cmp (_,(_,s1)) (_,(_,s2)) = compare_lex (OptParse.Opt.get Options.order) s1 s2 in
+  let solutions = List.sort ~cmp:cmp solutions in
+  Printf.printf "<table border=1>\n" ;
+  Printf.printf "<tr><tbody><th>Package</th>%s</tr>"
+  (String.concat "" (List.map (fun h -> Printf.sprintf "<th>%s</th>" h) hl));
+  Cudf.iter_packages (fun pkg ->
+    let pl = 
+      List.filter_map (fun (f,(h,_)) ->
+        try Some(f,Cudf_hashtbl.find h pkg) with Not_found -> None
+      ) solutions
+    in
+    if pl = [] then () else
+    if all_equal (List.map snd pl) then ()
+    else begin
+      let sl = List.map (fun (f,status) -> status_to_string pkg status) pl in
+      Printf.printf "<tr><td>%s %d</td>%s</tr>"
+      pkg.package pkg.version
+      (String.concat "" (List.map (fun s -> Printf.sprintf "<td>%s</td>" s) sl))
+    end
+  ) universe
+  ;
+  Printf.printf "</table>\n"
+;;
+
 
 let parse_univ f1 =
   match CudfAdd.load_cudf f1 with
@@ -173,20 +233,25 @@ let main () =
   |[u] -> (Printf.eprintf "You must specify at least a solution\n" ; exit 1)
   |u::l ->
       let (univ,req) = parse_univ u in
-      let sol_list =
+      let hl =
         List.filter_map (fun f ->
+          let (h,f) =
+            match Str.split (Str.regexp ":") f with
+            |[f] -> (f,f)
+            |[h;f] -> (h,f)
+            |_ -> assert false
+          in
           let (_,s,_) = CudfAdd.load_cudf f in
-          if check_sol univ req s then Some (f,s)
+          if check_sol univ req s then Some (h,(f,s))
           else (Printf.eprintf "%s is not a valid solution. Discarded\n" f ; None)
         ) l
       in
+      let (hl,sol_list) = List.split hl in
       let sol_tables = List.map (fun (f,s) -> (f,diff univ s)) sol_list in
-      print_diff univ sol_tables;
-      print_newline ();
-      List.iter (fun (f,(_,s)) ->
-        Printf.printf "%s :\n%s\n" f (solution_to_string s)
-      ) sol_tables
-
+      match OptParse.Opt.get Options.output with
+      |"txt" -> print_diff_txt univ hl sol_tables
+      |"html" -> print_diff_html univ hl sol_tables
+      |_ -> assert false 
 ;;
 
 main ();;
