ab33d857ab5a273ed8db5ad78878e5cbb62b1db6
Pietro Abate <Pietro.Abate@pps.jussieu.fr>
2010-05-27 13:57:12 +0000

    - cosmetic changes (new option --prefix)

--- applications/smallworld.ml
+++ applications/smallworld.ml
@@ -10,16 +10,8 @@
 (*  library, see the COPYING file for more information.                               *)
 (**************************************************************************************)
 
-(* Copyright (C) 2008 Pietro Abate <pietro.abate@pps.jussieu.fr>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * More info about small world networks
- * http://en.wikipedia.org/wiki/Small-world_network
- *)
+(* More info about small world networks
+  http://en.wikipedia.org/wiki/Small-world_network *)
 
 open Common
 
@@ -33,13 +25,14 @@ module Options =
     let components = StdOpt.store_true ()
     let smallworld = StdOpt.store_true ()
     let centrality = StdOpt.store_true ()
-    let outdir = StdOpt.str_option ()
+    let prefix = StdOpt.str_option ~default:"" ()
 
-    let options = OptParser.make ()
+    let description = "Compute the small world statistic of the dependency graph"
+    let options = OptParser.make ~description ()
     open OptParser
 
     add options ~short_name:'d' ~long_name:"debug" ~help:"Print various aggregate information" debug;
-    add options ~long_name:"outdir" ~help:"Send output to a file" outdir;
+    add options ~long_name:"prefix" ~help:"Prefix output fils with <prefix>" prefix;
     add options ~short_name:'g' ~long_name:"generic" ~help:"" generic;
     add options ~short_name:'p' ~long_name:"scatterplots" ~help:"" scatterplots;
     add options ~short_name:'c' ~long_name:"connectivity" ~help:"" connectivity;
@@ -63,10 +56,11 @@ let (>:::) s l = TestLabel(s, TestList(l))
 
 let print_test s f = (fun _ -> Printf.printf s f ; ())
 
-let rec run = function
-  |TestCase f -> ( f () ; print_newline ())
-  |TestList l -> ( print_newline () ; List.iter run l )
-  |TestLabel (l,t) -> ( Printf.printf "%s : " l ; run t )
+let rec run outch = function
+  |TestCase f -> ( f () ; Printf.fprintf outch "\n")
+  |TestList l -> ( Printf.fprintf outch "\n"; List.iter (run outch) l )
+  |TestLabel (l,t) -> ( Printf.fprintf outch "%s : " l ; (run outch) t )
+;;
 
 (**********************************)
 
@@ -86,45 +80,47 @@ let main () =
   if OptParse.Opt.get Options.debug then Boilerplate.enable_debug () ;
   let (universe,_,_) = Boilerplate.load_universe posargs in
   let gr = Defaultgraphs.SyntacticDependencyGraph.dependency_graph universe in
+  let prefix = OptParse.Opt.get Options.prefix in
+  let outch = if prefix = "" then stdout else open_out ( prefix ^ "stats" ) in
   let generic = "Generic" >::: [
-    "Vertex" >:: (fun _ -> Printf.printf "%d" (G.nb_vertex gr));
-    "Edges" >:: (fun _ -> Printf.printf "%d" (G.nb_edges gr));
+    "Vertex" >:: (fun _ -> Printf.fprintf outch "%d" (G.nb_vertex gr));
+    "Edges" >:: (fun _ -> Printf.fprintf outch "%d" (G.nb_edges gr));
     ]
   in
   let connectivity = "Connectivity" >::: [
-    "Average Out-Degree" >:: (fun _ -> Printf.printf "%0.2f" (S.averageOutDegree gr));
-    "Average In-Degree"  >:: (fun _ -> Printf.printf "%0.2f" (S.averageInDegree gr));
+    "Average Out-Degree" >:: (fun _ -> Printf.fprintf outch "%0.2f" (S.averageOutDegree gr));
+    "Average In-Degree"  >:: (fun _ -> Printf.fprintf outch "%0.2f" (S.averageInDegree gr));
     ]
   in
   let componentsSC = "Strongly Connected Components" >::: [
-    "Number of Components SC"  >:: (fun _ -> Printf.printf "%d" (S.numberComponentsSC gr));
-    "Average Components SC" >:: (fun _ -> Printf.printf "%0.2f" (S.averageComponentsSC gr));
-    "Larges Component SC" >:: (fun _ -> Printf.printf "%d" (S.largestComponentSC gr));
+    "Number of Components SC"  >:: (fun _ -> Printf.fprintf outch "%d" (S.numberComponentsSC gr));
+    "Average Components SC" >:: (fun _ -> Printf.fprintf outch "%0.2f" (S.averageComponentsSC gr));
+    "Larges Component SC" >:: (fun _ -> Printf.fprintf outch "%d" (S.largestComponentSC gr));
     ]
   in
   let componentsWC = "Weakly Connected Components" >::: [
-    "Number of Components WC"  >:: (fun _ -> Printf.printf "%d" (S.numberComponentsWC gr));
-    "Average Components WC" >:: (fun _ -> Printf.printf "%0.2f" (S.averageComponentsWC gr));
-    "Larges Component WC" >:: (fun _ -> Printf.printf "%d" (S.largestComponentWC gr));
+    "Number of Components WC"  >:: (fun _ -> Printf.fprintf outch "%d" (S.numberComponentsWC gr));
+    "Average Components WC" >:: (fun _ -> Printf.fprintf outch "%0.2f" (S.averageComponentsWC gr));
+    "Larges Component WC" >:: (fun _ -> Printf.fprintf outch "%d" (S.largestComponentWC gr));
     ]
   in
   let smallworld = "Small World" >::: [
-    "Clustering Coefficient" >:: (fun _ -> Printf.printf "%0.2f" (S.clustering gr));
-    "Average Shortest Path Length" >:: (fun _ -> Printf.printf "%0.2f" (S.averageShortestPathLength gr));
-    "Density" >:: (fun _ -> Printf.printf "%0.5f" (S.density gr));
-    "Average two step reach" >:: (fun _ -> Printf.printf "%0.2f" (S.averageTwoStepReach gr));
+    "Clustering Coefficient" >:: (fun _ -> Printf.fprintf outch "%0.2f" (S.clustering gr));
+    "Average Shortest Path Length" >:: (fun _ -> Printf.fprintf outch "%0.2f" (S.averageShortestPathLength gr));
+    "Density" >:: (fun _ -> Printf.fprintf outch "%0.5f" (S.density gr));
+    "Average two step reach" >:: (fun _ -> Printf.fprintf outch "%0.2f" (S.averageTwoStepReach gr));
     ]
   in
   let centrality = "Centrality" >::: [
-    "Centrality Out Degree" >:: (fun _ -> Printf.printf "%0.5f" (S.centralityOutDegree gr));
-    "Centrality In Degree" >:: (fun _ -> Printf.printf "%0.5f" (S.centralityInDegree gr));
+    "Centrality Out Degree" >:: (fun _ -> Printf.fprintf outch "%0.5f" (S.centralityOutDegree gr));
+    "Centrality In Degree" >:: (fun _ -> Printf.fprintf outch "%0.5f" (S.centralityInDegree gr));
     ]
   in
   let scatterplots = "Scattered Plots" >::: [
     "Scattered Plot In" >:: (fun _ ->
-      saveplot (S.scatteredPlotIn gr) "indegree.data" ; print_string "Done" );
+      saveplot (S.scatteredPlotIn gr) (prefix^"indegree.data") ; print_string "Done" );
     "Scattered Plot Out" >:: (fun _ ->
-      saveplot (S.scatteredPlotOut gr) "outdegree.data"; print_string "Done" );
+      saveplot (S.scatteredPlotOut gr) (prefix^"outdegree.data"); print_string "Done" );
 (*    "Hops Plot" >:: (fun _ ->
       saveplot (S.hopsplot gr 30) "hopsplot.data"; print_string "Done" );
       *)
@@ -137,7 +133,8 @@ let main () =
   if not (OptParse.Opt.get Options.components) then t := componentsWC :: componentsSC :: !t ;
   if not (OptParse.Opt.get Options.connectivity) then t := connectivity :: !t ;
   if not (OptParse.Opt.get Options.generic) then t := generic :: !t ;
-  run ("" >::: !t)
+  run outch ("" >::: !t);
+  close_out outch;
 ;;
 
 main () ;;
