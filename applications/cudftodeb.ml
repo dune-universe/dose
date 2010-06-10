a9b6fe1ea73a2d013076f42772412dc957e56d01
Pietro Abate <Pietro.Abate@pps.jussieu.fr>
2010-06-10 15:05:37 +0000

    - first attempt to convert back (deb)cudf documents to debian Packages + status + request

---
+++ applications/cudftodeb.ml
@@ -0,0 +1,77 @@
+
+
+open Cudf
+open ExtLib
+open Common
+
+module Options = struct
+  open OptParse
+
+  let debug = StdOpt.store_true ()
+
+  let description = "Report the broken packages in a package list"
+  let options = OptParser.make ~description ()
+
+  open OptParser
+  add options ~short_name:'d' ~long_name:"debug" ~help:"Print debug information" debug;
+end
+
+let convert universe =
+  let vr_RE = Pcre.regexp "(.*)--virtual" in
+  let f1 (pkgname,constr) =
+    if Pcre.pmatch ~rex: vr_RE pkgname then begin 
+      let s = Pcre.exec ~rex: vr_RE pkgname in
+      (Pcre.get_substring s 1, constr) 
+    end
+    else (pkgname,constr)
+  in
+  let f2 (pkgname,_) = not(Pcre.pmatch ~rex: vr_RE pkgname) in
+  Cudf.fold_packages (fun (status,pkglist) p ->
+    let p' = 
+      {p with
+      provides = List.map f1 p.provides;
+      conflicts = List.filter f2 p.conflicts;
+      depends = List.map (List.filter f2) p.depends;
+      pkg_extra = [] }
+    in
+    if p.installed then (p'::status,pkglist) else (status,p'::pkglist)
+  ) ([],[]) universe
+;;
+
+let main () =
+  at_exit (fun () -> Util.dump Format.err_formatter);
+  let posargs = OptParse.OptParser.parse_argv Options.options in
+  if OptParse.Opt.get Options.debug then Boilerplate.enable_debug () ;
+
+  let (status,pkglist,request) =
+    match posargs with
+    |[u] ->
+        Common.Util.print_info "%s" u ;
+        let (_,univ,req) = CudfAdd.load_cudf u in
+        let (s,l) = convert univ in
+        (s,l,req)
+    |_ -> (Printf.eprintf "You must specify one cudf document\n" ; exit 1)
+  in
+  let preamble = Cudf.default_preamble in
+
+  let status_oc = open_out "status" in
+  let packages_oc = open_out "Packages" in
+  let request_oc = open_out "Request" in
+  let status_ofr = Format.formatter_of_out_channel status_oc in
+  let packages_ofr = Format.formatter_of_out_channel packages_oc in
+  let request_ofr = Format.formatter_of_out_channel request_oc in
+
+  Cudf_printer.pp_preamble packages_ofr preamble;
+  Format.pp_print_newline packages_ofr ();
+  Cudf_printer.pp_packages packages_ofr pkglist;
+
+  Cudf_printer.pp_preamble status_ofr preamble;
+  Format.pp_print_newline status_ofr ();
+  Cudf_printer.pp_packages status_ofr status;
+
+  if request <> None then
+    Cudf_printer.pp_request request_ofr (Option.get request);
+  close_out status_oc ; close_out packages_oc ; close_out request_oc
+;;
+
+main ();;
