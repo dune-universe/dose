290e4e5e0eb7c5b7f9230272c095f9d1b5bcf67a
Roberto Di Cosmo <roberto@dicosmo.org>
2010-09-12 17:50:02 +0000

    Fixed error in join.sh

--- applications/join.sh
+++ applications/join.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+#
+# Quick hack to compare evolution when doing clustered vs. plain upgrades.
+# (C) Roberto Di Cosmo, Mancoosi Project 2010 - 
+#
 echo "Join.sh <plain> <clustered> joins the records for packages"
 echo "analysed for brokenn Impact Set in plain and clustered mode"
 echo " plain and clustered come from the analysis of strongpred,"
@@ -14,18 +17,18 @@ tmplain=/tmp/plain.$$
 tmpclustered=/tmp/clustered.$$
 
 
-sed -f cleanup.sed $plain | cut -f 4 -d ' ' --complement > $plain.csv
-sed -f cleanup.sed $clustered | cut -f 4 -d ' ' --complement > $clustered.csv
+sed -f cleanup.sed $plain | cut -f 4 -d ' ' --complement | awk -F, '{print $1","$3","$6","$2","$4","$5","$7","$8","$9}' > $plain.csv
+sed -f cleanup.sed $clustered | cut -f 4 -d ' ' --complement | awk -F, '{print $1","$3","$6","$2","$4","$5","$7","$8","$9}' > $clustered.csv
 
-sed -e 's/,/###JOIN###/' $plain.csv | sort -k 1b,1 > $tmplain
-sed -e 's/,/###JOIN###/' $clustered.csv | sort -k 1b,1 > $tmpclustered
+sed -e 's/,/###JOIN###/' $plain.csv | sed -e 's/,/###JOIN###/'  | sort -k 1b,1 > $tmplain
+sed -e 's/,/###JOIN###/' $clustered.csv | sed -e 's/,/###JOIN###/' | sort -k 1b,1 > $tmpclustered
 
 cat > plainclustered.csv<<EOF
-Package, current version,current cudf,new version, new cudf, #(IS(p)), broken plain, plain %, broken clustered, clustered %
+Package,current cudf,new cudf,current version,new version, #(IS(p)), broken plain, plain %, broken clustered, clustered %
 EOF
 
 
-join -j 1 -t, -o 0,1.2,1.4,1.5,1.7,1.6,1.8,2.6,2.8 $tmplain $tmpclustered | sed 's/###JOIN###/,/' >> plainclustered.csv
+join -j 1 -t, -o 0,1.2,1.4,1.6,1.5,1.7,2.5,2.7 $tmplain $tmpclustered | sed 's/###JOIN###/,/g' >> plainclustered.csv
 
 rm -f $tmplain $tmpclustered
 
