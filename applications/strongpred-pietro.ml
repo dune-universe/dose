bcd2285d5f6fc5007d631894eedced72f15afa01
Pietro Abate <Pietro.Abate@pps.jussieu.fr>
2010-11-11 18:12:44 +0000

    - small output quick in strongpred

--- applications/strongpred-pietro.ml
+++ applications/strongpred-pietro.ml
@@ -129,8 +129,8 @@ let pp_analysis fmt analysis =
   let (v,l) = analysis.target in
   Format.fprintf fmt "package: %a@," pp_package analysis.package;
   Format.fprintf fmt "target: \"%s\"@," v;
+  let l = Util.list_unique (List.filter ((<>) v) l) in
   if l <> [] then
-    let l = List.filter ((<>) v) l in
     Format.fprintf fmt "equiv: \"%s\"@," (String.concat " , " l);
   Format.fprintf fmt "answer: %a@," pp_answer analysis.answer;
   Format.fprintf fmt "info: %a@," pp_info analysis.answer;
@@ -257,7 +257,7 @@ let prediction sdgraph (universe1,from_cudf,to_cudf) =
         Util.Progress.progress predbar;
         (* compute a universe with the relevant packages in the cluster moved to version v *)
         let migration_list = Predictions.migrate conv_table version cluster in
-        let new_universe = Cudf.load_universe ((* FIXME: make this list unique!!! *) migration_list@universe_subset) in
+        let new_universe = Cudf.load_universe (migration_list@universe_subset) in
         let solver = Depsolver.load new_universe in
 
         (* for each package in the cluster, perform analysis *)
