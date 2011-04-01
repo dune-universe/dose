271fcf6d2110ca8b3717dbc45a1781b0e6f06fb7
Ralf Treinen <treinen@pps.jussieu.fr>
2011-04-01 12:25:39 +0000

    cosmetic issues in copyright headers

--- deb/version.ml
+++ deb/version.ml
@@ -1,5 +1,5 @@
 (**************************************************************************)
-(*  Copyright (C) 2009  Jaap Boender (jaap.boender@pps.jussieu.fr)        *)
+(*  Copyright (C) 2009  Jaap Boender <jaap.boender@pps.jussieu.fr>        *)
 (*                                                                        *)
 (*  This library is free software: you can redistribute it and/or modify  *)
 (*  it under the terms of the GNU Lesser General Public License as        *)
