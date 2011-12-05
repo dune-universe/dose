a2d7214b5918cf87242e28e01ae8c9eb54aa2799
Ralf Treinen <treinen@free.fr>
2011-12-05 16:02:02 +0100

    rewrite matching algorithm, split with default value.
    When source is neither all nor any then split both strings into os and
    host, and return the default os "linux" in case there is no os given.
    Fight the root of all evil in the world: premature optimisation!

--- deb/architecture.ml
+++ deb/architecture.ml
@@ -9,46 +9,24 @@
 (*  library, see the COPYING file for more information.                    *)
 (***************************************************************************)
 
-(*
-exception Architectures_inconsistent;;
-
-let bin_unify s1 s2 =
-  if s1=s2 || s1="all"
-  then s2
-  else if s2="all" then s1 else raise Architectures_inconsistent
-;;
-
-(bin_unify "all" "amd64");;
-(bin_unify "amd64" "all");;
-(bin_unify "all" "all");;
-(bin_unify "amd64" "i386");;
-
-*)
-
-open String;;
-
-let src_matches_arch source concrete =
-  let component_matches source concrete = (source="any" || source=concrete)
-  in
-  if source="all" || component_matches source concrete then true
-  else
-    (* the source architecture must consist of two parts: os and cpu *)
+let src_matches_arch source host =
+  (* matching of an OS or CPU component *)
+  let component_matches source host = (source="any" || source=host)
+  (* split an architecture string into OS and CPU, linux is default when
+     no OS is given. *)
+  and split arch =
     try
-      let i=index source '-' in
-      let source_os=sub source 0 i
-      and source_cpu= sub source (i+1) ((length source)-i-1) in
-      (* If the concrete architecture also splits into OS and Host
-	 then components must match. Otherwise we have an implicit
-	 OS which is linux *)
-      try
-	let j=index concrete '-' in
-	let concrete_os=sub concrete 0 j
-	and concrete_cpu=sub concrete (j+1) ((length concrete)-j-1)
-	in (component_matches source_os concrete_os) &&
-	  (component_matches source_cpu concrete_cpu)
-      with Not_found -> 
-	(source_os="linux" || source_os="any") &&
-	  component_matches source_cpu concrete
-    with Not_found -> false
+      let dash=String.index arch '-' in
+      (String.sub arch 0 dash,
+       String.sub arch (dash+1) ((String.length arch)-dash-1))
+    with
+	Not_found -> ("linux",arch)
+  in
+  if source="all" || source="any"  then true
+  else
+    let source_os,source_cpu = split source
+    and host_os,host_cpu = split host in
+    (component_matches source_os host_os) &&
+      (component_matches source_cpu host_cpu)
 ;;
       
