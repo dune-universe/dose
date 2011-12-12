eb04e59794b2c4ca4a8e9ea0c1cf4394f4f4b5a2
Ralf Treinen <treinen@free.fr>
2011-12-12 16:46:14 +0100

    update copyright headers. Minor changes in comments
    Use copyright header as of 2011/12/12. Reformat code to column width=80,
    fix some typos in comments, better use of ocamldoc tags.

--- deb/version.ml
+++ deb/version.ml
@@ -1,23 +1,28 @@
-(**************************************************************************)
-(*  Copyright (C) 2009-2011  Jaap Boender <jaap.boender@pps.jussieu.fr>   *)
-(*                           and Ralf Treinen <treinen@pps.jussieu.fr>    *)
-(*                                                                        *)
-(*  This library is free software: you can redistribute it and/or modify  *)
-(*  it under the terms of the GNU Lesser General Public License as        *)
-(*  published by the Free Software Foundation, either version 3 of the    *)
-(*  License, or (at your option) any later version.  A special linking    *)
-(*  exception to the GNU Lesser General Public License applies to this    *)
-(*  library, see the COPYING file for more information.                   *)
-(**************************************************************************)
+(******************************************************************************)
+(*  This file is part of the Dose library http://www.irill.org/software/dose  *)
+(*                                                                            *)
+(*  Copyright (C) 2011 Ralf Treinen <ralf.treinen@pps.jussieu.fr>             *)
+(*                                                                            *)
+(*  This library is free software: you can redistribute it and/or modify      *)
+(*  it under the terms of the GNU Lesser General Public License as            *)
+(*  published by the Free Software Foundation, either version 3 of the        *)
+(*  License, or (at your option) any later version.  A special linking        *)
+(*  exception to the GNU Lesser General Public License applies to this        *)
+(*  library, see the COPYING file for more information.                       *)
+(*                                                                            *)
+(*  Work developed with the support of the Mancoosi Project                   *)
+(*  http://www.mancoosi.org                                                   *)
+(*                                                                            *)
+(******************************************************************************)
 
 let is_digit = function
   | '0'..'9' -> true
   | _ -> false
 ;;
 
-(* (skip_while_from i f w) yields the index of the leftmost character in the string s, starting
- * from i, that does not satisfy the predicate f, or (length w) if no such index exists.
- *)
+(* [skip_while_from i f w] yields the index of the leftmost character
+ * in the string [s], starting from [i], that does not satisfy the
+ * predicate [f], or [length w] if no such index exists.  *)
 let skip_while_from i f w =
   let m = String.length w in
   let rec loop i =
@@ -25,10 +30,10 @@ let skip_while_from i f w =
     else if f w.[i] then loop (i + 1) else i
   in loop i
 ;;
-  
-(* splits a version into (epoch,rest), without the separating ':'. The epoch is delimited
- * by the leftmost occurrence of ':' in x, and is "" in case there is no ':' in x.
- *)
+
+(* splits a version into (epoch,rest), without the separating ':'. The
+ * epoch is delimited by the leftmost occurrence of ':' in x, and is ""
+ * in case there is no ':' in x.  *)
 let extract_epoch x =
   try
     let ci = String.index x ':' in
@@ -36,12 +41,12 @@ let extract_epoch x =
     and rest = String.sub x (ci + 1) (String.length x - ci - 1)
     in (epoch,rest)
   with
-  | Not_found -> ("",x)
+    | Not_found -> ("",x)
 ;;
 
-(* splits a version into (prefix,revision). The revision starts on the right-most
- * occurrence of '-', or is empty in case the version does not contain '-'.
- *) 
+(* splits a version into (prefix,revision). The revision starts on the
+ * right-most occurrence of '-', or is empty in case the version does
+ * not contain '-'.  *)
 let extract_revision x =
   try
     let di = String.rindex x '-' in
@@ -49,10 +54,11 @@ let extract_revision x =
     let after = String.sub x (di+1) (String.length x - di -1) in
     (before,after)
   with
-  | Not_found -> (x,"")
+    | Not_found -> (x,"")
 ;;
 
-(* character comparison uses a modified character ordering: '~' first, then letters, then anything else *)
+(* character comparison uses a modified character ordering: '~' first,
+   then letters, then anything else *)
 let compare_chars c1 c2 = match c1 with
   | '~' -> (match c2 with
       | '~' -> 0
@@ -66,14 +72,14 @@ let compare_chars c1 c2 = match c1 with
       | _ -> Char.compare c1 c2)
 ;;
 
-(* return the first index of x, starting from xi, of a nun-null character in x.
- * or (length x) in case x contains only 0's starting from xi on.
- *)
+(* return the first index of x, starting from xi, of a nun-null
+ * character in x.  or (length x) in case x contains only 0's starting
+ * from xi on.  *)
 let skip_zeros x xi = skip_while_from xi (fun c -> c = '0') x;;
 
-(* compare versions chunks, that is parts of version strings that are epoch, upstream version,
- * or revisision. Alternates string comparison and numerical comaprison.
- *)
+(* compare versions chunks, that is parts of version strings that are
+ * epoch, upstream version, or revisision. Alternates string comparison
+ * and numerical comaprison.  *)
 let compare_chunks x y =
   (* x and y may be empty *)
   let xl = String.length x
@@ -84,19 +90,24 @@ let compare_chunks x y =
     match (xi=xl,yi=yl) with (* which of x and y is exhausted? *)
       | true,true -> 0 
       | true,false -> 
-	  (* if y continues numerically than we have to continue by comparing numerically. In this case
-	   * the x part is interpreted as 0 (since empty). If the y part consists only of 0's
-	   * then both parts are equal, otherwise the y part is larger. If y continues non-numerically
-	   * then y is larger anyway, so we only have to skip 0's in the y part and check whether this
-	   * exhausts the y part.
-	   *)
-	let ys = skip_zeros y yi in if ys = yl then 0 else if y.[ys]='~' then 1 else -1
+	(* if y continues numerically than we have to continue by
+	 * comparing numerically. In this case the x part is
+	 * interpreted as 0 (since empty). If the y part consists
+	 * only of 0's then both parts are equal, otherwise the y
+	 * part is larger. If y continues non-numerically then y is
+	 * larger anyway, so we only have to skip 0's in the y part
+	 * and check whether this exhausts the y part.  *)
+	let ys = skip_zeros y yi in
+	if ys = yl then 0 else if y.[ys]='~' then 1 else -1
       | false,true -> (* symmetric to the preceding case *)
-	let xs = skip_zeros x xi in if xs = xl then 0 else if x.[xs]='~' then -1 else 1
+	let xs = skip_zeros x xi in
+	if xs = xl then 0 else if x.[xs]='~' then -1 else 1
       | false,false -> (* which of x and y continues numerically? *)
 	match (is_digit x.[xi], is_digit y.[yi]) with 
-	  | true,true -> (* both continue numerically. Skip leading zeros in the remaining parts, 
-			  * and then continue by comparing numerically. *)
+	  | true,true ->
+	    (* both continue numerically. Skip leading zeros in the
+	     * remaining parts, and then continue by
+	     * comparing numerically. *)
 	    compare_numerical (skip_zeros x xi) (skip_zeros y yi)
 	  | true,false -> (* '~' is smaller than any numeric part *)
 	    if y.[yi]='~' then 1 else -1
@@ -106,8 +117,10 @@ let compare_chunks x y =
 	    let comp = compare_chars x.[xi] y.[yi]
 	    in if comp = 0 then loop_lexical (xi+1) (yi+1) else comp
   and compare_numerical xi yi =
-    assert (xi = xl || (xi < xl && x.[xi] <> '0')); (* leading zeros have been striped *) 
-    assert (yi = yl || (yi < yl && y.[yi] <> '0')); (* leading zeros have been striped *) 
+    assert (xi = xl || (xi < xl && x.[xi] <> '0'));
+    (* leading zeros have been stripped *) 
+    assert (yi = yl || (yi < yl && y.[yi] <> '0'));
+    (* leading zeros have been stripped *) 
     let xn = skip_while_from xi is_digit x (* length of numerical part *)
     and yn = skip_while_from yi is_digit y (* length of numerical part *)
     in 
@@ -115,17 +128,23 @@ let compare_chunks x y =
     in if comp = 0
       then (* both numerical parts have same length: compare digit by digit *)
 	loop_numerical xi yi yn
-      else (* if one numerical part is longer than the other we have found the answer
-	    * since leading 0 have been striped when switching to numerical comparison.
-	    *)
+      else
+	(* if one numerical part is longer than the other we have found the
+	 * answer since leading 0 have been striped when switching
+	 * to numerical comparison.  *)
 	comp
   and loop_numerical xi yi yn =
     assert (xi <= xl && yi <= yn && yn <= yl);
-    (* invariant: the two numerical parts that remain to compare are of the same length *)
+    (* invariant: the two numerical parts that remain to compare are
+       of the same length *)
     if yi=yn
-    then (* both numerical parts are exhausted, we switch to lexical comparison *)
+    then
+      (* both numerical parts are exhausted, we switch to lexical
+	 comparison *)
       loop_lexical xi yi
-    else (* both numerical parts are not exhausted, we continue comparing digit by digit *)
+    else
+      (* both numerical parts are not exhausted, we continue comparing
+	 digit by digit *)
       let comp = Char.compare x.[xi] y.[yi] 
       in if comp = 0 then loop_numerical (xi+1) (yi+1) yn else comp
   in loop_lexical 0 0
@@ -152,11 +171,13 @@ let equal (x : string) (y : string) =
   if x = y then true else (compare x y) = 0
 ;;
 
-(************** splitting and recomposing version strings ************************************)
+(************** splitting and recomposing version strings *******************)
 
 type version_analysis =
-  | Native of string*string*string (* epoch,upstream,binnmu *)
-  | NonNative of string*string*string*string (* epoch,upstream,revision,binnmu *)
+  | Native of string*string*string
+    (* epoch,upstream,binnmu *)
+  | NonNative of string*string*string*string
+    (* epoch,upstream,revision,binnmu *)
 ;;
 
 let extract_binnmu v =
@@ -186,7 +207,8 @@ let compose = function
   | NonNative("",upstream,revision,"") -> upstream^"-"^revision
   | NonNative(epoch,upstream,revision,"") -> epoch^":"^upstream^"-"^revision
   | NonNative("",upstream,revision,binnmu) -> upstream^"-"^revision^"+"^binnmu
-  | NonNative(epoch,upstream,revision,binnmu) -> epoch^":"^upstream^"-"^revision^"+"^binnmu
+  | NonNative(epoch,upstream,revision,binnmu) ->
+    epoch^":"^upstream^"-"^revision^"+"^binnmu
 ;;
 
 let strip_epoch_binnmu v =
@@ -195,7 +217,7 @@ let strip_epoch_binnmu v =
     | NonNative(_,upstream,revision,_) -> upstream^"-"^revision
 ;;
 
-(*********************************************************************************************)
+(****************************************************************************)
 (* for the deprecated interface  *)
  
 let split s = match decompose s with
