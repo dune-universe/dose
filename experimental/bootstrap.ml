d93eb1c12ef150e1ae3d187dcb9bf6adda1fe7e0
Pietro Abate <pietro.abate@pps.jussieu.fr>
2012-03-02 12:22:24 +0100

    add feedback arc set algorithm

--- experimental/bootstrap.ml
+++ experimental/bootstrap.ml
@@ -24,6 +24,8 @@ let warning fmt = Util.make_warning __FILE__ fmt
 let debug fmt = Util.make_debug __FILE__ fmt
 let fatal fmt = Util.make_fatal __FILE__ fmt
 
+let progressbar = Util.Progress.create "fas"
+
 module Boilerplate = BoilerplateNoRpm
 module Src = Sources
 module Pkg = Packages
@@ -38,9 +40,10 @@ module Options = struct
   let native_arch = StdOpt.str_option ()
   let foreign_archs = Boilerplate.str_list_option ()
   
-  let base_system = Boilerplate.str_list_option ()
+  let base_system = Boilerplate.vpkglist_option ()
 
-  (* let checkonly = Boilerplate.vpkglist_option ()
+  (*
+  let checkonly = Boilerplate.vpkglist_option ()
   add options ~long_name:"checkonly" ~help:"Check only these package" checkonly;
   *)
 
@@ -59,6 +62,7 @@ module Options = struct
 end
 
 let main () =
+  let iteration = ref 0 in
 
   let posargs = OptParse.OptParser.parse_argv Options.options in
   
@@ -66,7 +70,7 @@ let main () =
   Boilerplate.enable_debug (OptParse.Opt.get Options.verbose);
   
   (* enable a selection of progress bars *)
-  Boilerplate.enable_bars (OptParse.Opt.get Options.progress) [] ;
+  Boilerplate.enable_bars (OptParse.Opt.get Options.progress) ["fas"] ;
 
   (* enable a selection of timers *)
   Boilerplate.enable_timers (OptParse.Opt.get Options.timers) [];
@@ -87,12 +91,6 @@ let main () =
   let foreign_archs =
     if OptParse.Opt.is_set Options.foreign_archs then 
       OptParse.Opt.get Options.foreign_archs
-    else []
-  in
-
-  let base_system =
-    if OptParse.Opt.is_set Options.base_system then 
-      OptParse.Opt.get Options.base_system
     else []
   in
 
@@ -128,9 +126,9 @@ let main () =
   let bl =
     List.map (fun pkg ->
       Debcudf.tocudf tables (
-        if List.mem pkg.Pkg.name base_system then 
+        (* if List.mem pkg.Pkg.name base_system then 
           {pkg with Pkg.depends = [] ; Pkg.conflicts = []}
-        else pkg
+        else *) pkg
       )
     ) binlist in
   let pkglist = sl@bl in
@@ -157,6 +155,37 @@ let main () =
   let universe_size = Cudf.universe_size universe in
   info "Total packages (source + binaries) %d" universe_size;
 
+  let base_system =
+    if OptParse.Opt.is_set Options.base_system then begin
+      List.flatten (
+        List.map (function
+          |(p,None) -> Cudf.lookup_packages universe p
+          |(p,Some(c,v)) ->
+              let filter = Some(c,to_cudf (p,v)) in
+              Cudf.lookup_packages ~filter universe p
+        ) (OptParse.Opt.get Options.base_system)
+      )
+    end else []
+  in
+
+
+  (*
+  let checklist =
+    if OptParse.Opt.is_set Options.checkonly then begin
+      info "--checkonly specified, consider all packages as background packages";
+      List.flatten (
+        List.map (function
+          |(p,None) -> Cudf.lookup_packages universe p
+          |(p,Some(c,v)) ->
+              let filter = Some(c,snd(to_cudf (p,v))) in
+              Cudf.lookup_packages ~filter universe p
+        ) (OptParse.Opt.get Options.checkonly)
+      )
+    end else []
+  in
+  *)
+
+
   if OptParse.Opt.is_set Options.dump then begin
     let oc = open_out (OptParse.Opt.get Options.dump) in
     info "Dumping Cudf file";
@@ -168,105 +197,40 @@ let main () =
     - build dependency list
   *)
 
-  let module G = Defaultgraphs.PackageGraph.G in
-  let module D = Defaultgraphs.PackageGraph.D in
+  let module PG = Defaultgraphs.PackageGraph in
+  let module G = PG.G in
+  let module D = PG.D in
   let module C = Graph.Components.Make(G) in
-  let g = G.create () in
-  List.iter (fun pkg ->
-      let sn = try Cudf.lookup_package_property pkg "source" with Not_found -> fatal "WTF source" in
-      let sv = try Cudf.lookup_package_property pkg "sourcenumber" with Not_found -> fatal "WTF sourcenumber"in
-    try
-      let src = Cudf.lookup_package universe (CudfAdd.encode ("src"^Src.sep^sn),to_cudf (sn,sv)) in
-      (* find an installation set of the build dependencies : the smallest ? *)
-      (* get the bin list, add the src package and ask for an
-       * installation set of this src in this universe *)
-      (* add to the graph all the runtime dependencies that are
-       * in this installation set *)
-      if not (G.mem_vertex g src) then begin
-        let req = Diagnostic_int.Sng (CudfAdd.vartoint universe src) in
-        let res = Depsolver_int.solve solver req in
-        let closure =
-          match res with
-          |Diagnostic_int.Success f_int ->
-              List.map (CudfAdd.inttovar universe) (f_int ~all:true ())
-          |_ -> info "broken source %s %s " sn sv ; []
-        in
-        List.iter (fun pkg ->
-          G.add_vertex g pkg;
-          List.iter (fun vpkgs ->
-            let l = CudfAdd.resolve_deps universe vpkgs in
-            List.iter (fun q ->
-              if List.mem q closure then begin
-                (* info "%s -> %s" (CudfAdd.string_of_package pkg) (CudfAdd.string_of_package q); *)
-                G.add_edge g pkg q
-              end;
-            ) l;
-          ) pkg.Cudf.depends
-        ) closure;
-      end;
-      (* info "%s -> %s" (CudfAdd.string_of_package pkg) (CudfAdd.string_of_package src); *)
-      G.add_edge g pkg src;
-    with Not_found -> begin
-      warning "The source package %s %s associated to the bin package %s is missing" 
-      sn sv (CudfAdd.string_of_package pkg);
-    end
-  ) bl;
-  (* this is the graph of all sources and build dependencies and their
-   * runtime dependencies *)
 
-  let reduce_bin g =
-    List.iter (fun src ->
-      if G.mem_vertex g src then
-        (* iter on all build-deps of a source *)
-        G.iter_succ (fun pkg ->
-          (* if the build-dep does not have any dependencies itself
-           * then remove the edge *)
-          if List.length pkg.Cudf.depends = 0 then
-            G.remove_edge g src pkg
-        ) g src
-    ) sl
+  let copy_graph g =
+    let g1 = G.create () in
+    G.iter_edges_e (fun e -> G.add_edge_e g1 e) g;
+    G.iter_vertex (fun v -> G.add_vertex g1 v) g;
+    g1
   in
 
-  let reduce_src g =
-    List.iter (fun bin ->
-      if G.mem_vertex g bin then
-        match G.succ g bin with
-        |[] when G.out_degree g bin = 0 -> G.remove_vertex g bin;
-        |[] -> ()
-        |l -> 
-            List.iter (fun src ->
-              if List.mem src sl && G.out_degree g src = 0 then
-                begin
-                  (* if this binary depends on a source package that does not have
-                   * any build dependencies then we can safely remove it from the
-                   * graph and recompile it *)
-                  Printf.printf "source %s can be compiled\n" (CudfAdd.string_of_package src); 
-                  G.remove_edge g bin src;
-                  G.remove_vertex g src;
-                  if List.length l = 1 then
-                    G.remove_vertex g bin
-                end
-            ) l
-    ) bl
-  in
-
-  let reduce_loops g =
-    let scc = C.scc_array g in
-    Array.iter (fun l ->
-      let broken = ref false in
-      if List.length l > 1 then info "Loop detected";
-      List.iter (fun pkg ->
-        if not !broken && List.mem pkg sl then begin
-          let l = G.succ g pkg in
-          let q = List.hd l in
-          G.remove_edge g pkg q;
-          info "arbitrarly breaking loop between %s and %s" 
-          (CudfAdd.string_of_package pkg)
-          (CudfAdd.string_of_package q);
-          broken := true;
-        end
-      ) l;
-    ) scc;
+  let fas sg =
+    info "subgraph (%d)" (G.nb_vertex sg);
+    let to_list t = Hashtbl.fold (fun k _ acc -> k::acc) t [] in
+    Util.Progress.set_total progressbar (G.nb_vertex sg) ;
+    let g = copy_graph sg in
+    let f = Hashtbl.create 1023 in
+    let vertex_set = Stack.create () in
+    G.iter_vertex (fun v -> Stack.push v vertex_set) g ;
+    info "before loop";
+    while G.nb_vertex g > 0 do
+      Util.Progress.progress progressbar;
+      let v = Stack.pop vertex_set in
+      if G.in_degree g v < G.out_degree g v then
+        G.iter_pred (fun v1 -> Hashtbl.add f (v1,v) ()) g v
+      else
+        G.iter_succ (fun v2 -> Hashtbl.add f (v,v2) ()) g v
+      ;
+      (* this removes all incoming and outgoing arcs to v *)
+      G.remove_vertex g v
+    done;
+    Util.Progress.reset progressbar;
+    to_list f
   in
 
   let print_dot s g =
@@ -275,12 +239,168 @@ let main () =
     close_out oc
   in
 
-  print_dot "reduce-start.dot" g;
+  let add_source g src =
+    if not (G.mem_vertex g src) then begin
+      let req = Diagnostic_int.Sng (CudfAdd.vartoint universe src) in
+      let res = Depsolver_int.solve solver req in
+      (* effectively here we select ONE solution to satsfy the build 
+       * dependencies of this package. There can be many of these solutions
+       * and a backtrack step might be required. This simplification
+       * make this algorithm not exaustive... *)
+      let closure =
+        match res with
+        |Diagnostic_int.Success f_int ->
+            List.map (CudfAdd.inttovar universe) (f_int ~all:true ())
+        |_ -> info "broken source %s " (CudfAdd.string_of_package src) ; []
+      in
+      List.iter (fun bin ->
+        (* if the source package depends on a package in the base system,
+         * we do not add this package *)
+        if not (CudfAdd.equal bin src) then
+          G.add_edge g src bin
+          (* source -> bin (build dep) *)
+      ) closure
+    end
+  in
 
-  let iteration = ref 0 in
+  let get_source pkg =
+    let sn = try Cudf.lookup_package_property pkg "source" with Not_found -> fatal "WTF source" in
+    let sv = try Cudf.lookup_package_property pkg "sourcenumber" with Not_found -> fatal "WTF sourcenumber"in
+    try 
+      Cudf.lookup_package universe (CudfAdd.encode ("src"^Src.sep^sn),to_cudf (sn,sv))
+    with Not_found -> begin
+      warning "The source package %s %s associated to the bin package %s is missing" 
+      sn sv (CudfAdd.string_of_package pkg);
+      raise Not_found
+    end
+  in
+
+  (* find an installation set of the build dependencies : the smallest ? *)
+  (* get the bin list, add the src package and ask for an
+   * installation set of this src in this universe *)
+  (* add to the graph all the runtime dependencies that are
+   * in this installation set *)
+  let g = G.create () in
+  List.iter (fun bin ->
+    try 
+      let src = get_source bin in
+      begin try add_source g src with Not_found -> () end;
+      (* bin -> source (belongs to) *)
+      G.add_edge g bin src;
+
+    with Not_found -> () (* from get_source *)
+  ) bl ;
+  
+  let remove_source g src =
+    G.iter_pred (fun bin ->
+      (* info "Remove binary %s" (CudfAdd.string_of_package bin); *)
+      G.remove_vertex g bin
+    ) g src;
+    info "Remove source %s" (CudfAdd.string_of_package src);
+    G.remove_vertex g src
+  in
+
+  let reduce g =
+    List.iter (fun src ->
+      if G.mem_vertex g src then
+        if G.out_degree g src = 0 then
+          remove_source g src
+    ) sl
+  in
+
+  let remove_base_packages g l =
+    List.iter (fun pkg ->
+      let src = 
+        if List.mem pkg bl then
+          get_source pkg
+        else pkg
+      in
+      remove_source g src
+    ) l
+  in
+ 
+  (*
+  let f g =
+    let module VS = 
+      Set.Make (struct
+        type t = G.V.t
+        let compare p q = compare (G.in_degree g q) (G.in_degree g p)
+      end) 
+    in
+
+    let s = List.fold_right VS.add bl VS.empty in
+    VS.iter (fun p ->
+      Printf.printf "%s (%d)\n" (CudfAdd.string_of_package p) (G.in_degree g p);
+    ) s
+  in
+  *)
+
+  let subgraph g l =
+    let sg = G.create () in
+    List.iter (fun src_up ->
+      if String.starts_with src_up.Cudf.package "src%3a" then
+        G.iter_succ (fun pkg -> 
+          if String.starts_with pkg.Cudf.package "src%3a" then
+            fatal "%s -> %s" (CudfAdd.string_of_package src_up) (CudfAdd.string_of_package pkg);
+          let sn = try Cudf.lookup_package_property pkg "source" with Not_found -> fatal "WTF source" in
+          let sv = try Cudf.lookup_package_property pkg "sourcenumber" with Not_found -> fatal "WTF sourcenumber"in
+          try
+            let src_down = Cudf.lookup_package universe (CudfAdd.encode ("src"^Src.sep^sn),to_cudf (sn,sv)) in
+            if not (CudfAdd.equal src_up src_down) then
+              G.add_edge sg src_up src_down
+          with Not_found -> info "missing %s %s ?" sv sv
+        ) g src_up
+    ) l;
+    sg
+  in
+
+  (* break dependency loops. This should use a heuristic of some kind or
+   * ask for user intervention *)
+  let reduce_loops g =
+    let scc = C.scc_array g in
+    info "SCC # %d" (Array.length scc);
+    Array.iteri (fun i l ->
+      if List.length l > 1 then begin 
+        info "Loop detected (%d)" (List.length l);
+        (*
+        info "Loop : %s" (
+          String.join " -> " (List.map (fun pkg -> 
+            CudfAdd.string_of_package pkg
+          ) l));
+          *)
+        let sg = subgraph g l in
+        info "subgraph";
+        let subname = Printf.sprintf "sub-%d-%d.dot" !iteration i in
+        print_dot subname sg;
+
+        let feedback_set = fas sg in
+        info "arcs to remove %d" (List.length feedback_set);
+        List.iter (fun (v1,v2) ->
+          info "Remove %s -> %s" 
+          (CudfAdd.string_of_package v1)
+          (CudfAdd.string_of_package v2);
+          G.remove_edge sg v1 v2
+        ) feedback_set;
+        let subname = Printf.sprintf "sub-%d-%d-fas.dot" !iteration i in
+        print_dot subname sg;
+
+      end
+    ) scc;
+  in
+
+  print_dot "reduce-start.dot" g;
+  remove_base_packages g base_system;
+  print_dot "reduce-base.dot" g;
+  reduce g;
+  print_dot "reduce-1.dot" g;
+  reduce g;
+  print_dot "reduce-2.dot" g;
+  reduce_loops g;
+
+  (*
   while G.nb_vertex g > 0 do
 
-    info "Nodes %d (this should descrease)" (G.nb_vertex g);
+    info "Nodes %d %d (this should descrease)" (G.nb_vertex g) (G.nb_edges g);
     info "Reduce bin (#%d)" !iteration;
     reduce_bin g;
     print_dot (Printf.sprintf "reduce-%d-bin.dot" !iteration) g;
@@ -293,6 +413,7 @@ let main () =
     incr iteration;
 
   done
+*)
  
  
   (* parse the list of packages to include in the base system and
