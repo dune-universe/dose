4bcf51af1b90c537a09b18305603008999967597
Pietro Abate <pietro.abate@pps.jussieu.fr>
2014-02-09 16:54:42 +0100

    Rework of the applications unit tests
    - All tests are listed in a file dose-tests.list
    - Results are checked ignoring changes related to the output order
    - New dependency to the python yaml parser
    - Still few broken tests

--- applications/dose-tests.py
+++ applications/dose-tests.py
@@ -6,208 +6,133 @@ import difflib
 import uuid
 import os,sys,time
 import argparse
+from itertools import groupby, ifilter
+import yaml, urllib
+import filecmp
+import cStringIO
 
-# TODO
-# dominators-graph.native  
-# smallworld.native  strong-deps.native
+def convert(d) :
+    def aux(e) :
+        if isinstance(e, dict) :
+            return frozenset(sorted([(k,aux(v)) for (k,v) in e.items()]))
+        elif isinstance(e, list) :
+            return frozenset(sorted([aux(v) for v in e]))
+        else :
+            return e
+    return sorted([aux(e) for e in d])
 
-verbose = 0
+def parse822(f) :
+    cnf_fields = ['conflict','depends','provides','recommends']
 
-def diff_files(fromfile,tofile):
-    n = 3 #context lines
-    fromdate = time.ctime(os.stat(fromfile).st_mtime)
-    todate = time.ctime(os.stat(tofile).st_mtime)
-    fromlines = open(fromfile, 'U').readlines()
-    tolines = open(tofile, 'U').readlines()
-    diff = difflib.unified_diff(fromlines, tolines, fromfile, tofile, 
-            fromdate, todate, n=n)
-    l = list(diff)
-    if l :
-        #sys.stdout.writelines(l)
-        return False
-    else :
+    def cnf(k,s) :
+        if k in cnf_fields :
+            l = s.split(',')
+            ll = map(lambda s : s.split('|'), l)
+            return ll
+        else :
+            return s
+
+    records = []
+    for empty, record in groupby(ifilter(lambda s: not s.startswith('#'),open(f)), key=str.isspace):
+        if not empty:
+            l = map(lambda s : tuple(s.split(': ')), record)
+            l = map(lambda (k,v) : (k,v.rstrip()), l)
+            # we ignore the preamble here ...
+            if ('preamble' not in l[0]) and (len(l) > 0):
+                pairs = ((k, cnf(k,v.strip())) for k,v in l)
+                records.append((pairs))
+
+    l = sorted([sorted(e) for e in records])
+    return [frozenset(e) for e in l]
+
+def parseyaml(f) :
+    print "yaml %s" % f
+    data = yaml.load(open(f))
+    report = data.get('report', [])
+    l = convert(report)
+    return l
+
+def parsetext(f) :
+    l = sorted(open(f).readlines())
+    return [frozenset(sorted(e)) for e in l]
+
+def diff_aux(expectedfile,resultfile,parser):
+    if filecmp.cmp(expectedfile,resultfile) :
         return True
+    else :
+        expected = parser(expectedfile)
+        result = parser(resultfile)
+        matcher = difflib.SequenceMatcher(None, expected, result)
+        if matcher.ratio() == 1.0 :
+            print "Warning ! Expected result and actual result are not identical."
+            print "The order is not the same."
+            return True
+        else :
+            if verbose > 2 :
+                diff = difflib.unified_diff(open(expectedfile).readlines(),open(resultfile).readlines())
+                sys.stdout.writelines(list(diff))
+            return False
 
-def test_application(self,expected_file,cmd):
+def diff_yaml(expectedfile,resultfile):
+    return diff_aux(expectedfile,resultfile, parseyaml)
+
+def diff_822(expectedfile,resultfile):
+    return diff_aux(expectedfile,resultfile,parse822)
+
+def diff_text(expectedfile,resultfile):
+    return diff_aux(expectedfile,resultfile,parsetext)
+    
+def test_application(self,expected_file,cmd,diff):
     uid = uuid.uuid1()
     if not os.path.exists("tmp"):
         os.makedirs("tmp")
 
     output_file = "tmp/%s.cudf" % uid
     output = open(output_file,'w')
-    if verbose == 2:
-        print " ".join(cmd)
-    #print expected_file
     p = Popen(cmd, stdout=output)
     p.communicate()
-    d = diff_files(output_file,expected_file)
+    d = diff(expected_file,output_file)
     output.close()
     os.remove(output_file)
     self.assertTrue(d)
 
 class DoseTests(unittest.TestCase):
-    def test_apt_cudf(self):
-        expected_file = "tests/applications/dose-tests/apt-cudf-install-i386"
-        cmd = ["./apt-cudf.native", "--conf", "doc/apt-cudf/apt-cudf.conf", "-e", "--solver=aspcud", "tests/deb/edsp/install-sarge-etch.edsp"]
-        test_application(self,expected_file,cmd)
+    def __init__(self, test):
+        super(DoseTests, self).__init__()
+        self.description = test['Description']
+        self.expected = test['Expected'] 
+        self.cmd = test['Cmd'].split(' ') + test['Input'].split(' ')
+        if test['Type'] == '822' :
+            self.difftype = diff_822
+        elif test['Type']  == 'yaml' :
+            self.difftype = diff_yaml
+        elif test['Type']  == 'text' :
+            self.difftype = diff_text
+        else :
+            self.difftype = diff_text
+    def shortDescription(self):
+        return "Description = " + self.description + "\n" + ("Cmd = ") + " ".join(self.cmd) + "\nExpected file = %s" % self.expected + "\n"
+    def runTest(self):
+        test_application(self,self.expected,self.cmd,self.difftype)
 
-    def test_apt_cudf_textlive(self):
-        expected_file = "tests/applications/dose-tests/apt-cudf-install-textlive"
-        cmd = ["./apt-cudf.native", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/textlive.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_same_provides_multi(self):
-        expected_file = "tests/applications/dose-tests/multiarch-same-provides-multi"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-same-provides-multi.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_same_provides(self):
-        expected_file = "tests/applications/dose-tests/multiarch-same-provides"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-same-provides.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_same_half(self):
-        expected_file = "tests/applications/dose-tests/multiarch-same-half"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-same-half.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_same_conflicts(self):
-        expected_file = "tests/applications/dose-tests/multiarch-same-conflicts"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-same-conflicts.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_same_conflictsi_virtual(self):
-        expected_file = "tests/applications/dose-tests/multiarch-same-conflicts-virtual"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-same-conflicts-virtual.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_same_no_deps(self):
-        expected_file = "tests/applications/dose-tests/multiarch-same-no-deps"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-same-no-deps.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_none(self):
-        expected_file = "tests/applications/dose-tests/multiarch-none"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-none.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_foreign(self):
-        expected_file = "tests/applications/dose-tests/multiarch-foreign"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-foreign.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_foreign(self):
-        expected_file = "tests/applications/dose-tests/multiarch-foreign-depends"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-foreign-depends.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_apt_cudf_multiarch_allowed(self):
-        expected_file = "tests/applications/dose-tests/multiarch-allowed"
-        cmd = ["./apt-cudf.native", "-e", "--conf", "doc/apt-cudf/apt-cudf.conf", "--solver=aspcud", "tests/deb/edsp/multiarch-allowed.edsp"]
-        test_application(self,expected_file,cmd)
-
-    def test_failure_distcheck(self):
-        expected_file = "tests/applications/dose-tests/distcheck_test_failure"
-        cmd = ["./distcheck.native","-f","-e","deb://tests/DebianPackages/sid.packages.bz2"]
-        test_application(self,expected_file,cmd)
-
-    def test_success_distcheck(self):
-        expected_file = "tests/applications/dose-tests/distcheck_test_success"
-        cmd = ["./distcheck.native","-s","deb://tests/DebianPackages/lenny.packages.bz2"]
-        test_application(self,expected_file,cmd)
-
-    # we consider essential packages and we print everything
-    def test_checkonly_distcheck(self):
-        expected_file = "tests/applications/dose-tests/distcheck_test_checkonly"
-        cmd = ["./distcheck.native", "--checkonly", "3dchess", "deb://tests/DebianPackages/sid.packages.bz2", "-s", "-e"]
-        test_application(self,expected_file,cmd)
-
-    # we consider essential packages but we print only the code of 3dchess
-    def test_checkonly_minimal_distcheck(self):
-        expected_file = "tests/applications/dose-tests/distcheck_test_minimal_checkonly"
-        cmd = ["./distcheck.native", "-m", "--checkonly", "3dchess", "deb://tests/DebianPackages/sid.packages.bz2", "-s", "-e"]
-        test_application(self,expected_file,cmd)
-
-    # we **do not** consider essential packages. The result in this case is semantically equal to
-    # test_checkonly_minimal_distcheck, but syntactically different ...
-    def test_checkonly_ignore_essential_distcheck(self):
-        expected_file = "tests/applications/dose-tests/distcheck_test_ignore_essential_checkonly"
-        cmd = ["./distcheck.native", "--deb-ignore-essential", "--checkonly", "3dchess", "deb://tests/DebianPackages/sid.packages.bz2", "-s", "-e"]
-        test_application(self,expected_file,cmd)
-
-    # XXX add test for checkonly + failure
-
-    def test_checkonly_multiarch_distcheck(self):
-        expected_file = "tests/applications/dose-tests/distcheck_test_checkonly_multiarch"
-        cmd = ["./distcheck.native", "--checkonly", "3dchess:amd64", "deb://tests/DebianPackages/sid.packages.bz2", "-s", "-e", "--deb-native-arch", "amd64"]
-        test_application(self,expected_file,cmd)
-
-    def test_ignore_essential_distcheck(self):
-        expected_file = "tests/applications/dose-tests/distcheck_test_ignore_essential"
-        cmd = ["./distcheck.native","--deb-ignore-essential","-f","-e","deb://tests/DebianPackages/sid.packages.bz2"]
-        test_application(self,expected_file,cmd)
-
-    def test_failure_outdated(self):
-        expected_file = "tests/applications/dose-tests/outdated_failure"
-        cmd = ["./outdated.native","-f","-e","tests/DebianPackages/sid.packages.bz2"]
-        test_application(self,expected_file,cmd)
-
-    def test_ceve_cnf(self):
-        expected_file = "tests/applications/dose-tests/ceve_cnf"
-        cmd = ["./ceve.native","-T","cnf","deb://tests/DebianPackages/sid.packages.bz2"]
-        test_application(self,expected_file,cmd)
-
-    def test_ceve_cone_dot(self):
-        expected_file = "tests/applications/dose-tests/ceve_cone_dot"
-        cmd = ["./ceve.native","-T","dot","-c", "3dchess", "deb://tests/DebianPackages/sid.packages.bz2"]
-        test_application(self,expected_file,cmd)
-
-    def test_ceve_cone_multiarch_dot(self):
-        expected_file = "tests/applications/dose-tests/ceve_cone_multiarch_dot"
-        cmd = ["./ceve.native","-T","dot","-c", "3dchess:amd64", "--deb-native-arch", "amd64", "-t", "deb", "tests/DebianPackages/sid.packages.bz2"]
-        test_application(self,expected_file,cmd)
-
-    def test_deb_buildcheck(self):
-        expected_file = "tests/applications/dose-tests/deb-buildcheck-output"
-        cmd = ["./deb-buildcheck.native", "--failures", "--explain", "--latest", "--deb-native-arch=amd64", "tests/DebianPackages/Sid-amd64-Packages-050812.bz2", "tests/DebianPackages/Sid-Sources-050812.bz2"]
-        test_application(self,expected_file,cmd)
-
-#    def test_deb_buildcheck_cross(self):
-#        expected_file = "tests/applications/dose-tests/apt-get-build-dep-armel-results"
-#        cmd = ["./deb-buildcheck.native", "--failures", "--successes", "--latest", "--deb-native-arch=amd64", "--deb-foreign-archs=armel", "--deb-host-arch=armel", "tests/DebianPackages/Sid-amd64-Packages-050812.bz2", "tests/DebianPackages/Sid-armel-Packages-050812.bz2", "tests/DebianPackages/Sid-Sources-050812.bz2"]
-#        if verbose == 2:
-#            print " ".join(cmd)
-#        import yaml, urllib
-#        p = Popen(cmd, stdout=PIPE)
-#        data = yaml.load(p.communicate()[0])
-#        apt_result = dict()
-#        with open(expected_file) as f:
-#            for line in f:
-#                k, v = line.split()
-#                apt_result[k] = v
-#        # see bug#683786 for explanation of exceptions
-#        exceptions = [ 'bomberclone', 'libcatalyst-actionrole-acl-perl', 'obex-data-server', 'qdox', 'renattach', 'trueprint', 'worker' ]
-#        #t = open("test-results",'w')
-#        for l in data.get('report', []):
-#            k = urllib.unquote(l['package']).split(':')[-1]
-#            #print >>t, k,apt_result[k]
-#            if l['status'] == 'ok':
-#                self.assertTrue(apt_result[k] == "OK" or k in exceptions)
-#            else :
-#                self.assertTrue(apt_result[k] == "FAIL" or k in exceptions)
+def suite(f):
+    suite = unittest.TestSuite()
+    for stanza in parse822(f):
+        s = dict(stanza)
+        #print "Add %s" % s['Cmd']
+        suite.addTest(DoseTests(s))
+    return suite
 
 def main():
     global verbose
-    parser = argparse.ArgumentParser(description='description of you program')
-    parser.add_argument('-v', '--verbose', action='store_const', const=2)
-    parser.add_argument('-d', '--debug', action='store_true')
-    parser.add_argument('-pwd', type=str, nargs=1, help="dose root directory")
+    parser = argparse.ArgumentParser(description='Unit test for Dose applications')
+    parser.add_argument('-v', '--verbose', type=int, nargs=1, default=2) 
+    parser.add_argument('inputfile', type=str, nargs=1, help="test file")
     args = parser.parse_args()
 
     verbose = args.verbose
 
-    suite = unittest.TestLoader().loadTestsFromTestCase(DoseTests)
-    unittest.TextTestRunner(verbosity=args.verbose).run(suite)
+    unittest.TextTestRunner(verbosity=args.verbose).run(suite(args.inputfile[0]))
 
 if __name__ == '__main__':
     main()
