cc00a8fa3007448afc5ed648cf6d780fced5b0f9
Pietro Abate <pietro.abate@pps.jussieu.fr>
2015-02-02 19:00:08 +0100

    More work on the website (almost done)

--- doc/webpages/extract-ocamldoc.py
+++ doc/webpages/extract-ocamldoc.py
@@ -3,22 +3,28 @@
 import os,sys,time
 import argparse
 
-def extract(html_doc):
+def extract(html_doc,man=False):
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(html_doc)
-    for a in soup.findAll('a'):
-        l = a['href'][:-5]
-        a['href'] = "../" + l + "/" + "index.html"
+    if man :
+        description = soup('h1',id='DESCRIPTION',limit=1)[0].find_next_sibling()
+        print description
+
+    if man is False :
+        for a in soup.findAll('a'):
+            l = a['href'][:-5]
+            a['href'] = "../" + l + "/" + "index.html"
      
     print soup.body
 
 def main():
     parser = argparse.ArgumentParser(description='Ocamldoc body extractor')
     parser.add_argument('inputfile', type=str, nargs=1, help="test file")
+    parser.add_argument('-m', '--man', action='store_true', default=False)
     args = parser.parse_args()
  
     f = args.inputfile[0]
-    extract(open(f))
+    extract(open(f),args.man)
 
 if __name__ == '__main__':
     main()
