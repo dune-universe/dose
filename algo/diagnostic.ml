18406c5f0a6c13ec29ffe4dbea6f357ce42db534
Pietro Abate <pietro.abate@pps.jussieu.fr>
2015-03-15 20:37:02 +0100

    Revert bump in output-version and sync tests submodule

--- algo/diagnostic.ml
+++ algo/diagnostic.ml
@@ -88,7 +88,7 @@ let default_result n = {
   statistic = Hashtbl.create 17
 }
 
-let pp_out_version fmt = Format.fprintf fmt "output-version: 1.1@.";;
+let pp_out_version fmt = Format.fprintf fmt "output-version: 1.0@.";;
 
 (** given a list of dependencies, return a list of list containg all
  *  paths in the dependency tree starting from [root] *)
