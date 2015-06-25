a0fc558b9588ad1f40581abcb6952d24da7fa5f3
Pietro Abate <pietro.abate@pps.jussieu.fr>
2015-06-25 10:24:34 +0200

    Fix compilation problem with ocamlgraph 1.8.6
    This change assures backward compatibility with ocamlgraph < 1.8.6
    via conditional compilation

--- algo/dominators.ml
+++ algo/dominators.ml
@@ -101,7 +101,11 @@ let dominators_tarjan graph =
   ) graph;
 
   Util.Timer.start tjntimer;
+#ifdef OCAMLGRAPH1.8.6
+  let module Dom = Dominator.Make_graph(G) in
+#else
   let module Dom = Dominator.Make(G) in
+#endif
   let idom = Dom.compute_all graph start_pkg in
   let domgr = idom.Dom.dom_graph () in
   Util.Timer.stop tjntimer ();
